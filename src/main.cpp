c98125bff2e4bcae09d266c36885edb7e6bfeb47
Phat Tang <tlphat18@apcs.vn>
2024-08-30 19:39:41 +0700

    feat: update main function to load the dictionary

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,34 @@
 
 #include <iostream>
 #include <fstream>
+#include <cstring>
 
-int main() {
-    BloomFilter bloom_filter;
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Too few arguments." << std::endl;
+        return 1;
+    }
 
-    std::ifstream in("dict.txt");
-    bloom_filter.LoadDictionary(in);
-    in.close();
+    if (std::strcmp("-build", argv[1]) == 0) {
+        if (argc != 3) {
+            std::cerr << "Incorrect number of arguments, expecting 1 parameter for build file." << std::endl;
+            return 1;
+        }
 
-    std::ofstream os("words.bf", std::ios::binary);
-    bloom_filter.SaveBinary(os);
-    os.close();
+        BloomFilter bloom_filter;
 
-    std::cout << bloom_filter.Exists("word") << std::endl;
+        std::ifstream in(argv[2]);
+        bloom_filter.LoadDictionary(in);
+        in.close();
 
-    return 0;
+        std::ofstream os("words.bf", std::ios::binary);
+        bloom_filter.SaveBinary(os);
+        os.close();
+
+        return 0;
+    }
+
+    std::cerr << "Unsupported operation." << std::endl;
+
+    return 1;
 }
